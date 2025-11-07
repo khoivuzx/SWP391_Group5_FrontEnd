cd4d714cda5d2c3f47edb943b2fe94d5ae6161af
WinBT <ducbtse181675@fpt.edu.vn>
2025-11-07 10:34:55 +0700

    staff

--- EV-Battery-Swap/src/config.js
+++ EV-Battery-Swap/src/config.js
@@ -1,5 +1,5 @@
 // src/config.js
 // Cấu hình endpoint API cho toàn bộ app
 
-const API_BASE_URL = "https://5d85c48a22f6.ngrok-free.app";
+const API_BASE_URL = "https://253581cdd947.ngrok-free.app";
 export default API_BASE_URL;
